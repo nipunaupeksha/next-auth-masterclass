4ce77f1991b6ee6f13566b819dd77486b0b9898e
nipunaupeksha <nipuna.upeksha@gmail.com>
2024-12-21 13:20:31 +0200

    implement: login functionality

---
+++ components/auth/login-button.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  mode?: "model" | "redirect";
+  asChild?: boolean;
+}
+
+export const LoginButton = ({
+  children,
+  mode = "redirect",
+  asChild = false,
+}: LoginButtonProps) => {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push("/auth/login");
+  };
+
+  if (mode === "model") {
+    return <span>TODO: Implement Modal</span>;
+  }
+
+  return (
+    <span onClick={onClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
+};
