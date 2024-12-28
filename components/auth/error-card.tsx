c7cd3773366ca1d715afaf043873e06d755c47f7
nipunaupeksha <nipuna.upeksha@gmail.com>
2024-12-28 19:18:37 +0200

    add: error card

--- components/auth/error-card.tsx
+++ components/auth/error-card.tsx
@@ -1,16 +1,16 @@
-import { Header } from "@/components/auth/header";
-import { BackButton } from "@/components/auth/back-button";
-import { Card, CardFooter, CardHeader } from "@/components/ui/card";
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { BsExclamationTriangle } from "react-icons/bs";
 
 export const ErrorCard = () => {
   return (
-    <Card className="w-[400px] shadow-md">
-      <CardHeader>
-        <Header label="Oops! Something went wrong!" />
-      </CardHeader>
-      <CardFooter>
-        <BackButton label="Back to login" href="/auth/login" />
-      </CardFooter>
-    </Card>
+    <CardWrapper
+      headerLabel="Oops! Something went wrong!"
+      backButtonHref="/auth/login"
+      backButtonLabel="Back to login"
+    >
+      <div className="w-full items-center flex justify-center">
+        <BsExclamationTriangle className="text-destructive" />
+      </div>
+    </CardWrapper>
   );
 };
