276b61064b0cc1d23c2af1aa765a2754aedd90ee
nipunaupeksha <nipuna.upeksha@gmail.com>
2024-12-28 14:03:20 +0200

    setup: next-auth

--- auth.ts
+++ auth.ts
@@ -1,7 +1,15 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+// import GitHub from "next-auth/providers/github";
+import authConfig from "@/auth.config";
+import { db } from "@/lib/db";
 
 export const {
   handlers: { GET, POST },
   auth,
-} = NextAuth({ providers: [GitHub] });
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
