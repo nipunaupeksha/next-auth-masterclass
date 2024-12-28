cfab53638ad71df020458c1a42caaa03e3ca8609
nipunaupeksha <nipuna.upeksha@gmail.com>
2024-12-28 04:36:37 +0200

    add: back button

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/card";
 import { Header } from "@/components/auth/header";
 import { Social } from "@/components/auth/social";
+import { BackButton } from "@/components/auth/back-button";
 
 interface CardWrapperProps {
   children: React.ReactNode;
@@ -32,9 +33,12 @@ export const CardWrapper = ({
       <CardContent>{children}</CardContent>
       {showSocial && (
         <CardFooter>
-          <Social/>
+          <Social />
         </CardFooter>
       )}
+      <CardFooter>
+        <BackButton label={backButtonLabel} href={backButtonHref} />
+      </CardFooter>
     </Card>
   );
 };
