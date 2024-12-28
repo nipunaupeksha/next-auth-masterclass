6e806255d1c58668826e32014eb194656ba2282e
nipunaupeksha <nipuna.upeksha@gmail.com>
2024-12-28 23:08:31 +0200

    add: resend for emails

--- actions/register.ts
+++ actions/register.ts
@@ -7,6 +7,7 @@ import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -33,6 +34,8 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   });
 
   const verificationToken = await generateVerificationToken(email);
+
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
 
   return { success: "User created!" };
 };
