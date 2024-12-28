b47e3779a8235fa6537449c300b397ce77656a77
nipunaupeksha <nipuna.upeksha@gmail.com>
2024-12-28 14:21:03 +0200

    setup: next-auth

---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,12 @@
+import { auth } from "@/auth";
+
+const SettingsPage = async () => {
+  const session = await auth;
+  return (
+    <div>
+      <div>{JSON.stringify(session)}</div>
+    </div>
+  );
+};
+
+export default SettingsPage;
